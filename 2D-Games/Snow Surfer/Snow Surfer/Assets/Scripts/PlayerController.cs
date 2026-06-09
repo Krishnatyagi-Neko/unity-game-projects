7c53285b96ecd0477b6a5a022a2ff1b26c4a078c
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-06-10 00:18:08 +0530

    Added Player Movements Using Unity Current Input System
    In This i Added Player Movement Mostly the rotations as that is only needed in this game using Current Modern Unity Input System.
    And we Implemented Rotation using Physics of RigidBody2D By Using Torque

---
+++ 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    InputAction moveAction;
+    Rigidbody2D myRigidBody2D;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.x < 0)
+        {
+            myRigidBody2D.AddTorque(torqueAmount);
+            print("hy");
+        }
+        else if (moveVector.x > 0)
+        {
+            myRigidBody2D.AddTorque(-torqueAmount);
+        }
+        
+    }
+}
