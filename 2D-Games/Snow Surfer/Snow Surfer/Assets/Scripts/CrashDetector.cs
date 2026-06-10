8c4142c3b87fefe126e90a599ebe6fd5e2bc6865
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-06-11 01:07:18 +0530

    Added Trigger for Particle System
    In This Commit I Added Particle System at Required instances such as when player crashed then only play the particle effect and same with finish line.

--- 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/CrashDetector.cs
+++ 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/CrashDetector.cs
@@ -5,17 +5,20 @@ public class CrashDetector : MonoBehaviour
 {
 
 
-    [SerializeField] float ResDelay = 1f;    
+    [SerializeField] float ResDelay = 1f;
+    [SerializeField] ParticleSystem crashparticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerindex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerindex)
         {
             Invoke("ReloadScene",ResDelay);
+            crashparticles.Play();
         }
     }
     void ReloadScene()
     {
+
         SceneManager.LoadScene(0);
     }
 }
--- 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/FinishLine.cs
+++ 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/FinishLine.cs
@@ -4,13 +4,14 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float ResDelay = 1f;
-
+    [SerializeField] ParticleSystem finishparticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerindex = LayerMask.NameToLayer("Player");
         if(collision.gameObject.layer == layerindex)
         {
             Invoke("ReloadScene",ResDelay);
+            finishparticles.Play();
         }
     }
 
