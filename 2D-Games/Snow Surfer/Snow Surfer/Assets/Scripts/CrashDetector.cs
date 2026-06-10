274402a149d79cf190e7c03bb4198098a28b6679
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-06-11 00:45:43 +0530

    Added Particle System
    Added Particle System for when player crosses Finish Line or get Craches.

--- 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/CrashDetector.cs
+++ 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/CrashDetector.cs
@@ -3,15 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+
+
+    [SerializeField] float ResDelay = 1f;    
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerindex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerindex)
         {
-            Invoke("ReloadSceneCrash",1f);
+            Invoke("ReloadScene",ResDelay);
         }
     }
-    void ReloadSceneCrash()
+    void ReloadScene()
     {
         SceneManager.LoadScene(0);
     }
