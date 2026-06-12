7288395fec87b684a7911ef905fe1694a652df0c
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-06-12 11:30:56 +0530

    Added a Boost Mechanism Using FindFirstObjectOfType
    By This Method I Added a Temporary Boost whenever the player will press up arrow key the character will get a speed.
    
    Though it have some loopholes like it won't wok in air as our speed is controlled by surfacecollider2D
    But for now i think it's Pretty Good

--- 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/PlayerController.cs
+++ 2D-Games/Snow Surfer/Snow Surfer/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    
     [SerializeField] float torqueAmount = 1f;
     InputAction moveAction;
     Rigidbody2D myRigidBody2D;
+    Vector2 moveVector;
+    SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] float BaseSpeed = 15f;
+    [SerializeField] float BoostSpeed = 200f;
+
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void FixedUpdate()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+        
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         if (moveVector.x < 0)
         {
@@ -25,6 +38,18 @@ public class PlayerController : MonoBehaviour
         {
             myRigidBody2D.AddTorque(-torqueAmount);
         }
-        
     }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = BoostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = BaseSpeed;
+        }
+    }
+
 }
