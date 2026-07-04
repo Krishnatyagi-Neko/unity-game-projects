568c40f20406100c372cf8bdfe4846feadf2760b
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-07-04 23:39:22 +0530

    Added Unity Input System & Horizontal Player Movement
    Setted up unity input system and then setted up player movement just for the horizontal direction
    and also altered y direction to linearvelocity given by rigidbody.

---
+++ 2D-Games/Tilevania Summit/Tilevania Summit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    [SerializeField] float movespeed = 5f;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x*movespeed, myRigidBody.linearVelocity.y);
+        myRigidBody.linearVelocity = playerVelocity;
+    }
+    
+
+
+
+
+}
