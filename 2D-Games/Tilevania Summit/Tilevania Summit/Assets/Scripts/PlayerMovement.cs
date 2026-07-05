3da6e82b3c80b542498965acad15d25be9b2ce8e
Krishna Tyagi <krishna.tyagi.dev@gmail.com>
2026-07-05 11:38:46 +0530

    Added Animation States
    In This i introduced Animation State
    Using Running Animation When Actually running and using Idling Animation when not doing anything.
    Toggling this on and off with a bool.

--- 2D-Games/Tilevania Summit/Tilevania Summit/Assets/Scripts/PlayerMovement.cs
+++ 2D-Games/Tilevania Summit/Tilevania Summit/Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,21 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     [SerializeField] float movespeed = 5f;
+    Animator myAnimator;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
-        
         moveInput = value.Get<Vector2>();
     }
 
@@ -27,10 +29,21 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x*movespeed, myRigidBody.linearVelocity.y);
         myRigidBody.linearVelocity = playerVelocity;
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidBody.linearVelocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning",hasHorizontalSpeed);
+
+
     }
     
 
-
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(myRigidBody.linearVelocity.x) > Mathf.Epsilon;
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.linearVelocity.x), 1f);
+        }
+    }
 
 
 }
